3ee8b8a921e6d8e22393d08900013f6483187d78
uzakotim <timauzakov@gmail.com>
2023-05-05 13:31:39 +0200

    distnaces plot

--- combine_odometry.py
+++ combine_odometry.py
@@ -1,6 +1,8 @@
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import pandas
+import csv
+from math import sqrt
 def closest(l, R):
 
     from operator import itemgetter
@@ -11,7 +13,8 @@ def closest(l, R):
 
     return closeToR  
 
-
+def quadratic_distance(x,y,z,x_c,y_c,z_c):
+    return sqrt((x-x_c)**2 +(y-y_c)**2 +(z-z_c)**2)
 
 points1 = pandas.read_csv("./main_results_uav4/main.bag_slow_odom_uav4.csv")
 points2 = pandas.read_csv("./main_results_uav8/main.bag_slow_odom_uav8.csv")
@@ -19,19 +22,45 @@ points3 = pandas.read_csv("./main_results_uav11/main.bag_slow_odom_uav11.csv")
 centroid = pandas.read_csv("./main_centroid_uav4/main.bag_centroid_uav4.csv")
 
 t4 = centroid['time'].values
- 
-counter= 0
-for el in t4:
-# closest time stamp in points 1
-    timestamp = closest(points1['time'].values,el)
-    check = points1.loc[points1['time'] == timestamp]['time'].values[0]
-    if timestamp == check:
-        print("alright")
-    current_value_from_sf = centroid.loc[centroid['time'] == el]['x'].values[0]
-    if (current_value_from_sf != -10000000000.0):
-        print(current_value_from_sf)
-    counter +=1
-    if counter > 9:
-        break
-#print(len(t1),len(t2),len(t3),len(t4))
+last_centroid = [0,0,0]
+with open('./distances.csv', mode='w') as data_file:
+    data_writer = csv.writer(data_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    data_writer.writerow(['time', 'uav4', 'uav8', 'uav11'])
 
+    for el in t4:
+    # closest time stamp in points 1
+        timestamp_1 = closest(points1['time'].values,el)
+        timestamp_2 = closest(points2['time'].values,el)
+        timestamp_3 = closest(points3['time'].values,el)
+        
+        x_1 = points1.loc[points1['time'] == timestamp_1]['x'].values[0]
+        y_1 = points1.loc[points1['time'] == timestamp_1]['y'].values[0]
+        z_1 = points1.loc[points1['time'] == timestamp_1]['z'].values[0]
+        
+        x_2 = points2.loc[points2['time'] == timestamp_2]['x'].values[0]
+        y_2 = points2.loc[points2['time'] == timestamp_2]['y'].values[0]
+        z_2 = points2.loc[points2['time'] == timestamp_2]['z'].values[0]
+
+
+        x_3 = points3.loc[points3['time'] == timestamp_3]['x'].values[0]
+        y_3 = points3.loc[points3['time'] == timestamp_3]['y'].values[0]
+        z_3 = points3.loc[points3['time'] == timestamp_3]['z'].values[0]
+
+
+        x_c = centroid.loc[centroid['time'] == el]['x'].values[0]
+        y_c = centroid.loc[centroid['time'] == el]['y'].values[0]
+        z_c = centroid.loc[centroid['time'] == el]['z'].values[0]
+        # check = points1.loc[points1['time'] == timestamp]['time'].values[0]
+        # if timestamp == check:
+            # print("alright")
+        # current_value_from_sf = centroid.loc[centroid['time'] == el]['x'].values[0]
+        if (x_c != -10000000000.0):
+            last_centroid = [x_c,y_c,z_c]
+            # found centroid
+        else:
+            [x_c,y_c,z_c] = last_centroid
+        
+        dist1 = quadratic_distance(x_1,y_1,z_1,x_c,y_c,z_c)
+        dist2 = quadratic_distance(x_2,y_2,z_2,x_c,y_c,z_c)
+        dist3 = quadratic_distance(x_3,y_3,z_3,x_c,y_c,z_c)
+        data_writer.writerow([el, dist1, dist2, dist3])
---
+++ plot_distances.py
@@ -0,0 +1,21 @@
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import pandas
+import time
+distances = pandas.read_csv("./distances.csv")
+
+fig = plt.figure()
+# first points
+
+t = distances['time'].values
+min_t = min(t)
+t = [(x-min_t)*1e-9 for x in t]
+uav4  = distances['uav4'].values
+uav8  = distances['uav8'].values
+uav11 = distances['uav11'].values
+plt.xlabel("Time [sec]")
+plt.ylabel("Distance to the last observed centroid of objects [m]")
+plt.plot(t,uav4,t,uav8,t,uav11)
+plt.legend(['uav4','uav8','uav11'])
+# plt.legend(loc="upper left")
+plt.show()
